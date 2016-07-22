64ba2416b762ccb301edd22cf970ca6601a5a92f
Van Le <singularity@vannio.co.uk>
2016-07-22 19:25:17 +0100

    Add sass middleware

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     browserify = require('browserify-middleware'),
+    sass = require('node-sass-middleware'),
     nunjucks = require('nunjucks');
 var app = express();
 var port = process.env.NODE_ENV === 'test' ? 3000 : 8080;
@@ -17,6 +18,13 @@ app.use(express.static('./app/public'));
 
 // Javascript file bundling
 app.use('/scripts/bundle.js', browserify(__dirname + '/models/main.js'));
+
+// Sass compiling
+app.use(sass({
+  src: __dirname + '/sass',
+  dest: __dirname + '/public',
+  outputStyle: 'compressed'
+}));
 
 // Routes
 app.get('/', function (req, res) {
