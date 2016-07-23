25f8f7dbdb33984bd1ee6e4bcde01362ddc80e3f
Van Le <singularity@vannio.co.uk>
2016-07-23 23:37:11 +0100

    Refactor routes out into seperate files

---
+++ app/controllers/programmes.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    request = require('request');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.redirect('/programmes/a');
+});
+
+router.get('/:letter', function(req, res) {
+  var letter = req.params.letter.toLowerCase(),
+      page = req.query.page ? '?page=' + req.query.page : '';
+
+  var baseUrl = 'https://ibl.api.bbci.co.uk/ibl/v1/atoz/',
+      requestUrl = baseUrl + letter + '/programmes' + page;
+
+  request(requestUrl, function(err, response, body) {
+    var responseData;
+
+    if (err) responseData = { error: err };
+    else {
+      var data = JSON.parse(body).atoz_programmes;
+      var pages = data.count > data.per_page ? Math.ceil(data.count / data.per_page) : 1;
+
+      responseData = {
+        currentLetter: letter,
+        data: data,
+        pages: pages
+      };
+    }
+
+    res.render('programmes/index', responseData);
+  });
+});
+
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -1,16 +1,14 @@
 var express = require('express'),
     browserify = require('browserify-middleware'),
     sass = require('node-sass-middleware'),
-    nunjucks = require('nunjucks'),
-    request = require('request');
+    nunjucks = require('nunjucks');
 
 var app = express();
 var port = process.env.NODE_ENV === 'test' ? 3000 : 8080;
 
-// Template rendering
 app.set('view engine', 'njk');
 
-var env = nunjucks.configure('./app/views', {
+var env = nunjucks.configure(__dirname + '/views', {
   watch: true,
   express: app
 });
@@ -20,51 +18,19 @@ var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 env.addGlobal('letters', letters);
 
-// Static file routing
-app.use(express.static('./app/public'));
+app.use(express.static(__dirname + '/public'));
 
-// Javascript file bundling
 // app.use('/scripts/bundle.js', browserify(__dirname + '/models/main.js'));
 
-// Sass compiling
 app.use(sass({
   src: __dirname + '/sass',
   dest: __dirname + '/public',
   outputStyle: 'compressed'
 }));
 
-// Routes
-app.get('/', function (req, res) {
-  res.render('index')
-});
+app.use('/', require(__dirname + '/controllers/main'));
+app.use('/programmes', require(__dirname + '/controllers/programmes'));
 
-app.get('/programmes/:letter', function(req, response) {
-  var letter = req.params.letter.toLowerCase(),
-      page = req.query.page ? '?page=' + req.query.page : '';
-
-  var baseUrl = 'https://ibl.api.bbci.co.uk/ibl/v1/atoz/',
-      requestUrl = baseUrl + letter + '/programmes' + page;
-
-  request(requestUrl, function(err, res, body) {
-    if (err) {
-      response.render('programmes/index', {
-        error: err
-      });
-    }
-    else {
-      var data = JSON.parse(body).atoz_programmes;
-      var pages = data.count > data.per_page ? Math.ceil(data.count / data.per_page) : 1;
-
-      response.render('programmes/index', {
-        currentLetter: letter,
-        data: data,
-        pages: pages
-      });
-    }
-  });
-});
-
-// Listen for connections
 app.listen(port, function () {
   console.log(
     " ----------------------------------------\
