6b0d24153aab1ec7d64b2bbb7bfd46ebfe054014
Van Le <singularity@vannio.co.uk>
2016-07-23 08:07:39 +0100

    Move API-reading from client-side to server-side

--- app/models/main.js
+++
@@ -1,7 +0,0 @@
-var list20programmes = require('./list20programmes');
-
-function init(letter){
-  list20programmes(letter);
-}
-
-window.init = init;
--- app/server.js
+++ app/server.js
@@ -1,23 +1,30 @@
 var express = require('express'),
     browserify = require('browserify-middleware'),
     sass = require('node-sass-middleware'),
-    nunjucks = require('nunjucks');
+    nunjucks = require('nunjucks'),
+    request = require('request');
+
 var app = express();
 var port = process.env.NODE_ENV === 'test' ? 3000 : 8080;
 
 // Template rendering
+app.set('view engine', 'njk');
+
 var env = nunjucks.configure('./app/views', {
   watch: true,
   express: app
 });
-env.addGlobal('letters', 'abcdefghijklmnopqrstuvwxyz012345679'.split(''));
-app.set('view engine', 'njk');
+
+var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    letters.push('0-9');
+
+env.addGlobal('letters', letters);
 
 // Static file routing
 app.use(express.static('./app/public'));
 
 // Javascript file bundling
-app.use('/scripts/bundle.js', browserify(__dirname + '/models/main.js'));
+// app.use('/scripts/bundle.js', browserify(__dirname + '/models/main.js'));
 
 // Sass compiling
 app.use(sass({
@@ -31,15 +38,27 @@ app.get('/', function (req, res) {
   res.render('index')
 });
 
-app.get('/programmes/:letter', function(req, res) {
-  res.render('programmes/index', { letter: req.params.letter });
+app.get('/programmes/:letter', function(req, response) {
+  var letter = req.params.letter;
+
+  var baseUrl = 'https://ibl.api.bbci.co.uk/ibl/v1/atoz/',
+      requestUrl = baseUrl + letter + '/programmes';
+
+  request(requestUrl, function(err, res, body) {
+    var data = JSON.parse(body).atoz_programmes;
+
+    response.render('programmes/index', {
+      letter: letter,
+      data: data
+    });
+  });
 });
 
 // Listen for connections
 app.listen(port, function () {
   console.log(
-    "----------------------------------------\
-    \n** Listening on http://localhost:" + port +
-    " **\n----------------------------------------\n"
+    " ----------------------------------------\
+    \n ** Listening on \x1b[36mhttp://localhost:" + port +
+    "\x1b[0m **\n ----------------------------------------\n"
   );
 });
